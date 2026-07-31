fbd5c71dd4ca085f853e7b8d7ce44de6d878b492
Andy-Li15 <li1266700@gmail.com>
2026-07-31 17:17:07 -0400

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.java
@@ -0,0 +1,11 @@
+// use min heap to get kth largest
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i : nums) {
+            pq.add(i);
+            if (pq.size() > k) pq.remove();
+        }
+        return pq.remove();
+    }
+}
