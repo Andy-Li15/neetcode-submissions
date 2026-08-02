62dc84e7d023f685f52768932e6b6ee272d949fc
Andy-Li15 <li1266700@gmail.com>
2026-08-01 23:46:45 -0400

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,49 @@
+/*
+each time there is a palindrome, there are 2 options
+    partition the palindrome
+    don't partition the palindrome
+*/
+class Solution {
+    List<List<String>> result;
+    List<String> subset;
+
+    public List<List<String>> partition(String s) {
+        result = new ArrayList<>();
+        subset = new ArrayList<>();
+        getSubsets(s, 0, 1);
+        return result;
+    }
+
+    private void getSubsets(String s, int left, int right) {
+        if (right >= s.length()) {
+            String substring = s.substring(left, right);
+            if (isPalindrome(substring)) {
+                subset.add(substring);
+                result.add(new ArrayList<>(subset));
+                subset.remove(subset.size() - 1);
+            }
+            return;
+        }
+
+        String substring = s.substring(left, right);
+        if (isPalindrome(substring)) {
+            // try partitioning
+            subset.add(substring);
+            getSubsets(s, right, right + 1);
+            subset.remove(subset.size() - 1);
+        }
+        // don't partition
+        getSubsets(s, left, right + 1);
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
