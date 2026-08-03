be531de44532872735be8850a5a210efc1f80c97
Andy-Li15 <li1266700@gmail.com>
2026-08-02 21:18:18 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,44 @@
+class PrefixTree {
+
+    HashMap<Character, PrefixTree> map;
+    boolean isEnd;
+
+    public PrefixTree() {
+        map = new HashMap<>();
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        PrefixTree tree = this;
+        for (char c : word.toCharArray()) {
+            tree = tree.map.computeIfAbsent(c, ignored -> new PrefixTree());
+        }
+        tree.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        PrefixTree tree = this;
+        for (char c : word.toCharArray()) {
+            if (!tree.map.containsKey(c)) {
+                return false;
+            }
+            tree = tree.map.get(c);
+        }
+        if (tree.isEnd) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean startsWith(String prefix) {
+        PrefixTree tree = this;
+        for (char c : prefix.toCharArray()) {
+            if (!tree.map.containsKey(c)) {
+                return false;
+            }
+            tree = tree.map.get(c);
+        }
+        return true;
+    }
+}
